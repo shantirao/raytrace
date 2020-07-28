da5a43abda2e9986e1d5f7fca661bf69b437e69b
shantirao <shanti@shantirao.com>
2020-07-28 16:02:57 -0700

    Label surface in findFocus

--- matlab/findFocus.m
+++ matlab/findFocus.m
@@ -1,5 +1,5 @@
 function [focus] = findFocus(src,surfaces)
-%% find an exit pupil that is an optical conjugate of the first mirror
+%% find the plane where parallel rays intersect
 % inputs: surfaces is a cell array of optical surfaces, sourceAperture is a
 % structure describing the ray origins
 % outputs: pupil is the surface, index is where it goes in the surface 
@@ -11,15 +11,19 @@ options.segments = false;
 options.aperture = false;
 %% find an image by tracing from the source aperture. 
 
+ri = sourceColumn(src,2);
+ri = struct;
 ri.N = 2;
 ri.direction = repmat(src.direction,2,1);
 ri.position = [src.position;src.position+max(src.aperture/100)*src.local(1,:)]; % 1mm offset
 ri.valid = [true;true];
 ri.opl = [0;0];
+ri.chief =1;
 
 r = raytrace(ri,surfaces,options);
 r = r{end};
+focus.name = 'Focus';
 focus.position = lineIntersection(r.position,r.direction);
-focus.direction = -r.direction(1,:);
+focus.direction = -r.direction(r.chief,:);
 focus.local = surfaceLocal(focus);
 end
